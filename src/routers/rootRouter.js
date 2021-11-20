f47bb26c35402e9276c4743386e8bd5c38ae82cb
Yonghyun <yh47529722@gmail.com>
2021-11-20 19:49:23 +0900

    make frame

---
+++ src/routers/rootRouter.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { home } from "../controllers/rootController";
+const rootRouter = express.Router();
+
+rootRouter.get("/", home);
+
+export default rootRouter;
---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import morgan from "morgan";
+import rootRouter from "./routers/rootRouter";
+
+const app = express();
+const logger = morgan("dev");
+
+const PORT = 3500;
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use(express.urlencoded({extended:true}));
+app.use("/", rootRouter);
+
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
